5bef8ffb275dac4a1353ce63188a7ebf35285fe9
Tahsin Rashad <tahsin.rashad@selise.ch>
2023-07-15 21:08:15 +0600

    implemented ProductRepository

--- src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -31,27 +31,50 @@ namespace Catalog.API.Repositories
 
         public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
         {
-            throw new NotImplementedException();
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+
+            return await _context
+                            .Products
+                            .Find(filter)
+                            .ToListAsync();
         }
 
         public async Task<IEnumerable<Product>> GetProductByName(string name)
         {
-            throw new NotImplementedException();
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+
+            return await _context
+                            .Products
+                            .Find(filter)
+                            .ToListAsync();
         }
 
         public async Task CreateProduct(Product product)
         {
-            throw new NotImplementedException();
+            await _context.Products.InsertOneAsync(product);
         }
 
         public async Task<bool> UpdateProduct(Product product)
         {
-            throw new NotImplementedException();
+            var updateResult = await _context
+                                        .Products
+                                        .ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged
+                    && updateResult.ModifiedCount > 0;
+
         }
 
         public async Task<bool> DeleteProduct(string id)
         {
-            throw new NotImplementedException();
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            DeleteResult deleteResult = await _context
+                                                .Products
+                                                .DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged
+                && deleteResult.DeletedCount > 0;
         }
     }
 }
