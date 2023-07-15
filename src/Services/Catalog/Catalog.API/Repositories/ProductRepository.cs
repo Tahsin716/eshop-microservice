da06b219b621cf4e818c3eb0606b465f897cd4f4
Tahsin Rashad <tahsin.rashad@selise.ch>
2023-07-15 21:06:35 +0600

    getting products for repository implemented

--- src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Catalog.API.Data.Contracts;
 using Catalog.API.Entities;
 using Catalog.API.Repositories.Contracts;
+using MongoDB.Driver;
 
 namespace Catalog.API.Repositories
 {
@@ -12,37 +13,43 @@ namespace Catalog.API.Repositories
             _context = context;
         }
 
-        public Task<Product> GetProduct(string id)
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context
+                           .Products
+                           .Find(p => p.Id == id)
+                           .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context
+                            .Products
+                            .Find(p => true)
+                            .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Product>> GetProducts()
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        public async Task CreateProduct(Product product)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Product>> GetProductByName(string name)
+        public async Task<bool> UpdateProduct(Product product)
         {
             throw new NotImplementedException();
         }
 
-        public Task CreateProduct(Product product)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> UpdateProduct(Product product)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> DeleteProduct(string id)
+        public async Task<bool> DeleteProduct(string id)
         {
             throw new NotImplementedException();
         }
