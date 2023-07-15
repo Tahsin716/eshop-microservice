f30ea0cd29fbd4281b539656b6578c16c6c7a97b
Tahsin Rashad <tahsin.rashad@selise.ch>
2023-07-15 21:03:49 +0600

    implementing Repository pattern in Catalog.API for performing database operations on Products

---
+++ src/Services/Catalog/Catalog.API/Repositories/Contracts/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories.Contracts
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProducts();
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using Catalog.API.Data.Contracts;
+using Catalog.API.Entities;
+using Catalog.API.Repositories.Contracts;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Product> GetProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
