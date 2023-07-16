034f5ca9ba463e43da1c02d7d77665eac149997d
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-07-16 14:08:56 +0600

    Catalog controller added in catalog.api

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+    }
+}
