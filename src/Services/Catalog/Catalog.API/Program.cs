0bb5bf2c61b3ab7699208ab3b1645aa059d2e6ba
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-07-16 14:06:19 +0600

    registered services for catalog.api

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,13 @@
+using Catalog.API.Data;
+using Catalog.API.Data.Contracts;
+using Catalog.API.Repositories;
+using Catalog.API.Repositories.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
