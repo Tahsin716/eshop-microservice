5470f7bdd420e30269542917d8476a1f6431fb6b
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-07-17 19:40:46 +0600

    Added Microsoft.Extensions.Caching.StackExchangeRedis, also configured the connectionion string and service registratation

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
