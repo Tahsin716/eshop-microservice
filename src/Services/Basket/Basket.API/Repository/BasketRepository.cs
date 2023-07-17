ae30dc4b5a02c2a5cc2110d90fc5f7c55f9211ac
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-07-17 20:31:10 +0600

    Added IBasketRepository and BasketRepository implementation using DistributedCache

---
+++ src/Services/Basket/Basket.API/Repository/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repository.Contracts;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repository
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public BasketRepository(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<ShoppingCart?> GetBasket(string userName)
+        {
+            var basket = await _distributedCache.GetStringAsync(userName);
+
+            return string.IsNullOrEmpty(basket)
+                ? await Task.FromResult<ShoppingCart?>(null)
+                : JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task UpdateBasket(ShoppingCart basket)
+        {
+            await _distributedCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _distributedCache.RemoveAsync(userName);
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repository/Contracts/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Repository.Contracts
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart?> GetBasket(string userName);
+        Task UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
