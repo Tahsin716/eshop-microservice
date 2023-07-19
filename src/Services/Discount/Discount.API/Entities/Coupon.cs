2278a8c187496898d6656b66d6fef2debd900326
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-07-19 19:31:38 +0600

    Added Coupon Entity

---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace Discount.API.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Amount { get; set; }
+    }
+}
