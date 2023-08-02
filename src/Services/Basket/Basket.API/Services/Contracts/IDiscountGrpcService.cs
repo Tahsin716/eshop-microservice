ba009c400e667c7ec47a0eb36076fb6c0f400036
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-08-02 12:40:44 +0600

    discount service implemented

--- src/Services/Basket/Basket.API/Services/Contracts/IDiscountGrpcService.cs
+++ src/Services/Basket/Basket.API/Services/Contracts/IDiscountGrpcService.cs
@@ -1,6 +1,9 @@
-﻿namespace Basket.API.Services.Contracts
+﻿using Discount.gRPC.Protos;
+
+namespace Basket.API.Services.Contracts
 {
     public interface IDiscountGrpcService
     {
+        Task<CouponModel> GetCoupon(string productName);
     }
 }
--- src/Services/Basket/Basket.API/Services/DiscountGrpcService.cs
+++ src/Services/Basket/Basket.API/Services/DiscountGrpcService.cs
@@ -1,8 +1,21 @@
 ﻿using Basket.API.Services.Contracts;
+using Discount.gRPC.Protos;
 
 namespace Basket.API.Services
 {
     public class DiscountGrpcService : IDiscountGrpcService
     {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService;
+        }
+
+        public async Task<CouponModel> GetCoupon(string productName)
+        {
+            var discountRequest = new GetCouponRequest { ProductName = productName };
+            return await _discountProtoService.GetCouponAsync(discountRequest);
+        }
     }
 }
