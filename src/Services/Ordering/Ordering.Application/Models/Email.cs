6606ac4434b691483923f2486cf80ef7748e6286
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-08-04 20:27:47 +0600

    Infrastructure interface and models added

---
+++ src/Services/Ordering/Ordering.Application/Models/Email.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Models
+{
+    public class Email
+    {
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Models/EmailSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Models
+{
+    public class EmailSettings
+    {
+        public string ApiKey { get; set; } = string.Empty;
+        public string FromAddress { get; set; } = string.Empty;
+        public string FromName { get; set; } = string.Empty;
+    }
+}
