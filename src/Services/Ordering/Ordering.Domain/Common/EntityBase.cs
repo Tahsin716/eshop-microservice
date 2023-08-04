ad590681b2a9682dbd69dceb1855008a5139634d
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-08-04 19:50:59 +0600

    Added entitybase and valueobject of DDD

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public string Id { get; protected set; } = string.Empty;
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; } = string.Empty;
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
