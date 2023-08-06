bc36950e899e39e0e3a878ef1a00a3babf5e2395
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-08-06 20:30:09 +0600

    checkout command implemented

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommand : IRequest<int>
+    {
+        public string UserName { get; set; } = string.Empty;
+        public decimal TotalPrice { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+
+        public string CardName { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string Expiration { get; set; } = string.Empty;
+        public string CVV { get; set; } = string.Empty;
+        public int PaymentMethod { get; set; }
+    }
+}
