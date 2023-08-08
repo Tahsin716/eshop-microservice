9f751f1d73bee16377500acd0ddd94d8480681f1
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-08-08 12:28:21 +0600

    Exceptions implemented for order.application

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Ordering.Application.Exceptions
+{
+    [Serializable]
+    public class NotFoundException : ApplicationException, ISerializable
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+        }
+
+        protected NotFoundException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+        {
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.Results;
+using System.Runtime.Serialization;
+
+namespace Ordering.Application.Exceptions
+{
+    [Serializable]
+    public class ValidationException : ApplicationException, ISerializable
+    {
+        public IDictionary<string, string[]> Errors { get; }
+
+        public ValidationException()
+            : base("One or more validation failures have occurred.")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+            : this()
+        {
+            Errors = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+
+        protected ValidationException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+
+    }
+}
