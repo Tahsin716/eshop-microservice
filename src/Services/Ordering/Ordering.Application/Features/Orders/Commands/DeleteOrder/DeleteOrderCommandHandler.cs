4742eaea711c997b300bb3693376c545830bbca4
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-08-08 11:04:15 +0600

    DeleteOrderCommandHandler implemented

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistance;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand, Unit>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository,
+            ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+            
+            if (orderToDelete == null)
+            {
+                _logger.LogInformation($"No order found with order id: {request.Id}");
+                return Unit.Value;
+            }
+
+            await _orderRepository.DeleteAsync(orderToDelete);
+            _logger.LogInformation($"Order {orderToDelete.Id} is successfully deleted.");
+
+            return Unit.Value;
+        }
+    }
+}
