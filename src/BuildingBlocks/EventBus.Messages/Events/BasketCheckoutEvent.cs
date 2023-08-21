2a2e875b919695f0e6e92be9fa96a6b43cd62a7d
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-08-21 16:45:59 +0600

    BasketCheckoutEvent implemented

--- src/BuildingBlocks/EventBus.Messages/Events/BasketCheckoutEvent.cs
+++ src/BuildingBlocks/EventBus.Messages/Events/BasketCheckoutEvent.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EventBus.Messages.Events
+﻿namespace EventBus.Messages.Events
 {
-    internal class BasketCheckoutEvent
+    public class BasketCheckoutEvent : IntegrationBaseEvent
     {
+        public string UserName { get; set; } = string.Empty;
+        public decimal TotalPrice { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string ZipCode { get; set; } = string.Empty;
+
+        public string CardName { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string Expiration { get; set; } = string.Empty;
+        public string CVV { get; set; } = string.Empty;
+        public int PaymentMethod { get; set; }
     }
 }
