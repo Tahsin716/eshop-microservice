85f031a3936d947d4760ac990e83ebd3af21a1c1
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-08-21 19:24:16 +0600

    BasketMapper added

---
+++ src/Services/Basket/Basket.API/Mappers/BasketMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Basket.API.Entities;
+using EventBus.Messages.Events;
+
+namespace Basket.API.Mappers
+{
+    public class BasketMapper : Profile
+    {
+        public BasketMapper()
+        {
+            CreateMap<BasketCheckoutEvent, BasketCheckout>();
+        }
+    }
+}
