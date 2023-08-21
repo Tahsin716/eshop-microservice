0109648596780390513e6af0b1344ae1bf03b27e
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-08-21 16:42:35 +0600

    IntegrationBaseEvent implemented

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreateDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreateDate = createDate;
+        }
+    }
+}
