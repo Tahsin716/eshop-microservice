b9761da82afcd5a784327c0f97ba5a7ddc60c4b2
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-09-08 19:07:48 +0600

    updated programs.cs for ocelot config

--- src/ApiGateways/OcelotApiGateway/Program.cs
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -1,6 +1,26 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+builder.Logging.AddJsonConsole();
+
+builder.Configuration
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+await app.UseOcelot();
 
 app.Run();
