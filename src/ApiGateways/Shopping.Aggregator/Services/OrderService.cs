abbc2b2f03deb69a868a90fcc067a7cb4fdc73e2
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-09-12 19:30:36 +0600

    order service implemented

---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services.Contracts;
+
+namespace Shopping.Aggregator.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}
