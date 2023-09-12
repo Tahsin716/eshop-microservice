732d80ee0760d1d57cf62b3445abe3ff3c6158ba
Tahsin Rashad <tahsin.rashad716@gmail.com>
2023-09-12 18:55:45 +0600

    Contracts for shopping.aggregator added

---
+++ src/ApiGateways/Shopping.Aggregator/Services/Contracts/IBasketService.cs
@@ -0,0 +1,9 @@
+﻿using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services.Contracts
+{
+    public interface IBasketService
+    {
+        Task<BasketModel> GetBasket(string userName);
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/Contracts/ICatalogService.cs
@@ -0,0 +1,11 @@
+﻿using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services.Contracts
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+        Task<CatalogModel> GetCatalog(string id);
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+    }
+}
